f8d352bc8042f131283a44061e30bb7caa96425f
21038177 <21038177@LAPTOP-8VBOGQ9T>
2022-05-31 12:09:20 +0800

    first push to P00 repository in GitHub

---
+++ P00/test/HelloWorldTest.java
@@ -0,0 +1,37 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class HelloWorldTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void test() {
+		//fail("Not yet implemented");
+		System.out.println("This is the testcase for HelloWorld");
+		String str="This is the testcase for HelloWorld";
+		String str2="This is the testcase for HelloWorld";
+		assertEquals(str,str2);
+	}
+
+}
